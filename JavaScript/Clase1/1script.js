d81a097d2c2910ad7eed531161632a6d991cbd7f
EnriqueRodrigoP <enriquerodrigopardo@gmail.com>
2022-09-29 18:03:47 +0000

    CommitClase1

---
+++ JavaScript/Clase1/1script.js
@@ -0,0 +1,16 @@
+/*Usa esta pagina para javascript el profesor:
+https://javascript.info/*/
+/*Crear y declarar una variable*/ 
+let message;
+message="Soy una variable";
+/*Esto es para crear una alerta*/
+alert(message);
+/*Esto solo escribe en consola*/
+console.log('Hola mundo');
+/*Pregunta al que este usando que escriba una edad*/ 
+let age = prompt("Cuantos años tienes",100);
+
+/*Te sale una checkbox*/
+let isBoss=confirm("Eres el jefe?");
+
+
