63b70c122f90623975522585c6803a847a3d0930
EnriqueRodrigoP <enriquerodrigopardo@gmail.com>
2022-11-11 09:23:53 +0000

    continue

--- Express/ChatdeSocket/index.js
+++ Express/ChatdeSocket/index.js
@@ -9,7 +9,7 @@ var usernames = [];
 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
   function userNotExist(username,ID)
